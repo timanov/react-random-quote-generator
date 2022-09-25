4cf54294e56beab28347ab6c51f2d9a5fb604761
mikhail <mihailtimanov@gmail.com>
2022-09-25 22:08:43 +0300

    Доработка состояния

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [quote, setQuote] = useState("Hello");
+  const [author, setAuthor] = useState("World");
+
   return (
     <div className="App">
       <div className="quote">
-          <h2>New Quote</h2>
-          <small>-Auther-</small>
-      </div>
+          <h2>{quote}</h2>
+          <small>-{author}-</small>
+      </div><br />
       <button className='btn'>Generate New Quote</button>
     </div>
     
