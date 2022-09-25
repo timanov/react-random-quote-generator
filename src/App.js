1b2d81e4139d7bda68084c6535bb63ea57847a66
mikhail <mihailtimanov@gmail.com>
2022-09-25 22:31:11 +0300

    Добавление активности кнопки

--- src/App.js
+++ src/App.js
@@ -13,18 +13,28 @@ function App() {
       .then(
         (quote => {
            setQuote(quote.content);
-           setAuthor();
-          console.log(quote);
+           setAuthor(quote.author);
         })
       )
   },[]);
+
+  let fetchNewQuote = () => {
+    fetch("http://api.quotable.io/random")
+      .then(res => res.json())
+      .then(
+        (quote => {
+           setQuote(quote.content);
+           setAuthor(quote.author);
+        })
+      )
+  }
   return (
     <div className="App">
       <div className="quote">
           <h2>{quote}</h2>
           <small>-{author}-</small>
       </div><br />
-      <button className='btn'>Generate New Quote</button>
+      <button className='btn' onClick={fetchNewQuote}>Generate New Quote</button>
     </div>
     
   );
